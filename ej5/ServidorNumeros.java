21db090f8a5068d31296dceb0f9ca00914526275
M42 <mromang08@gmail.com>
2014-12-03 20:32:46 +0100

    Servidor leyendo y respondiendo

--- ej5/ServidorNumeros.java
+++ ej5/ServidorNumeros.java
@@ -2,9 +2,11 @@
 // Servidor Números
 // 
 
-
+import java.io.BufferedReader;
+import java.io.PrintWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -32,13 +34,53 @@ public class ServidorNumeros {
 
 class HebraServidor extends Thread {
     private Socket socketServicio;
-    
+    private BufferedReader inReader;
+    private PrintWriter outPrinter;
+
     // Constructor de una hebra, dado un socket.
     HebraServidor (Socket socketServicio) {
 	this.socketServicio = socketServicio;
+
+	// Obtiene los flujos de escritura/lectura
+	try {
+	    InputStreamReader isr = new InputStreamReader(socketServicio.getInputStream());
+	    inReader = new BufferedReader(isr); 
+	    outPrinter = new PrintWriter(socketServicio.getOutputStream(), true);
+	}
+	catch (IOException e) {
+	    System.err.println("Error al leer los flujos.");
+	}
     }
 
     public void run() {
+	Adivina adivinador = new Adivina();
+	Boolean finalizado = false;
 	
+	while (!finalizado) {
+	    // Lee un intento del cliente
+	    int resultado = 0;
+
+	    try {
+		int datoRecibido = Integer.parseInt(inReader.readLine());
+		resultado = adivinador.intento(datoRecibido);
+	    }
+	    catch (IOException e) {
+		System.err.println("Error al leer los flujos.");
+	    }
+
+	    // Envía la respuesta del servidor
+	    switch (resultado) {
+	    case 0:  outPrinter.println("¡Acierto!");
+	    case 1:  outPrinter.println("El número es mayor");
+	    case -1: outPrinter.println("El número es menor");
+	    }
+
+	    finalizado = (resultado == 0);
+
+	    // Intenta adivinar al cliente
+	    if (!finalizado) {
+		outPrinter.println(adivinador.estima());
+	    }
+	}
     }
 }
